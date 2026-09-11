f6dd279c6ce39d2584a632223edcc7938ec29c55
mukithasan232 <mdmukithasan429@gmail.com>
2026-09-11 08:46:13 +0600

    chore: update server.js and prisma tsconfig for vercel deploy

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 
 // Send index.html as the default route
-app.get('*', (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
